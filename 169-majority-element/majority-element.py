6c3c64b2189fcd38b2c9f2158ead748eda04ca18
Ray <ray12121212121212121212@gmail.com>
2026-09-11 20:13:05 -0400

    Time: 9 ms (57.91%) | Memory: 13.5 MB (74.38%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #ik there's a faster solution, but i'll stick to dictionaries for now
+        count = {}
+        
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+        
+        for num in count:
+            if count[num] > len(nums) //2:
+                return num
+        
